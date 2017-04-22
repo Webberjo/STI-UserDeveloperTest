cbe77da8e9245281be52d3c3723e1b3e35106593
Joseph Webber <Josephwebber@hotmail.com>
2017-04-22 19:00:31 -0300

    First design mockup based on wireframe.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,27 @@
 var app = angular.module('STI-User-Developer-Test', []);
 
-app.controller('MainController', ['$scope', function($scope) {
-
+app.controller('MainController', ['$http','$scope', function($http, $scope) {
+  $scope.card = [];
+  $scope.otherTitle = '';
+  
+  // Get client data from JSON file
+  $http({
+    method: 'GET',
+    url: 'assets/json/JaneDoe.json'
+  }).then(function(response) {
+    $scope.card = response.data;
+    $scope.card.title = 'Overview';
+    $scope.otherTitle = 'Details';
+    $scope.card.showOverview = true;
+  });
+  
+  $scope.toggleInfo = function() {
+    if ($scope.card.title == 'Overview') {
+      $scope.card.title = 'Details';
+      $scope.otherTitle = 'Overview';
+    } else {
+      $scope.card.title = 'Overview';
+      $scope.otherTitle = 'Details';
+    }
+  };
 }]);
